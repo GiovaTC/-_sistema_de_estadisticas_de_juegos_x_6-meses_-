306467321fc286a87b02a468c8f547389a72c4d1
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-16 15:47:17 -0500

    commit - script_GAME_RESULTS / .

---
+++ Services/GameService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStatsOracle.Services
+{
+    internal class GameService
+    {
+    }
+}
