000f56699a687dba1791a0d49c7876c0a7952792
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-16 15:52:03 -0500

    commit - GameResult / .

--- Models/GameResult.cs
+++ Models/GameResult.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameStatsOracle.Models
 {
-    internal class GameResult
+    public class GameResult
     {
+        public DateTime GameDate { get; set; }
+        public string PlayerName { get; set; }
+        public string GameName { get; set; }
+        public int Score { get; set; }
+        public string Result { get; set; }
     }
-}
+}   
