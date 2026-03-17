2520015c4ec740c1f0ce7ceb06f644ba71e853d2
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-17 15:47:41 -0500

    commit - Program .

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GameStatsOracle.Models;
+using GameStatsOracle.Services;
+using System;   
+
+namespace GameStatsOracle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameService gameService = new GameService();
+            StatisticsService statsService= new StatisticsService();
+
+            Console.WriteLine("registro de juego");
+
+            GameResult game = new GameResult();
+            game.GameDate = DateTime.Now;
+
+            Console.Write("jugador: ");
+            game.PlayerName = Console.ReadLine();
+
+            Console.Write("juego: ");
+            game.GameName = Console.ReadLine();
+
+            Console.Write("puntaje: ");
+            game.Score = int.Parse(Console.ReadLine());
+
+            Console.Write("resultado (win / lose): ");
+            game.Result = Console.ReadLine();
+
+            gameService.SaveGame(game);
+            Console.WriteLine("juego guardaro en oracle. ");
+
+            Console.WriteLine("\nestadisticas ultimos 6 meses:\n");
+
+            statsService.GetStatisticsLastSixMonths();
+        }
+    }
+}   
