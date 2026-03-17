46d653d3b66ab70b2d36ee944de368d57b1a9869
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-17 09:42:43 -0500

    commit - GameService / .

--- Services/GameService.cs
+++ Services/GameService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GameStatsOracle.Database;
+using GameStatsOracle.Models;
+using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,31 @@ using System.Threading.Tasks;
 
 namespace GameStatsOracle.Services
 {
-    internal class GameService
+    public class GameService
     {
+        private OracleConnectionManager connectionManager = new OracleConnectionManager();
+
+        public void SaveGame(GameResult game)
+        {
+            using (var connection = connectionManager.GetConnection())
+            {
+                connection.Open();
+
+
+                string sql = @"INSERT INTO GAME_RESULTS 
+                              (GAME_DATE, PLAYER_NAME, GAME_NAME, SCORE, RESULT) 
+                                VALUES (:date, :player, :game, :score, :result)";
+
+                OracleCommand cmd = new OracleCommand(sql, connection);
+
+                cmd.Parameters.Add(":date", game.GameDate);
+                cmd.Parameters.Add(":player", game.PlayerName);
+                cmd.Parameters.Add(":game", game.GameName);
+                cmd.Parameters.Add(":score", game.Score);
+                cmd.Parameters.Add(":result", game.Result);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
-}
+}   
