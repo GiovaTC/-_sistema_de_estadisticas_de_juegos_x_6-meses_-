190fa35a3a16e56340d249e14343d5dacaba1a4f
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-17 10:00:17 -0500

    commit - StatisticsService / .

--- Services/StatisticsService.cs
+++ Services/StatisticsService.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GameStatsOracle.Database;
+using Oracle.ManagedDataAccess.Client;
+using System;
 
 namespace GameStatsOracle.Services
 {
-    internal class StatisticsService
+    public class StatisticsService
     {
-    }
+        private OracleConnectionManager connectionManager = new OracleConnectionManager();
+
+        public void GetStatisticsLastSixMonths()
+        {
+            using (var connection = connectionManager.GetConnection())
+            {
+                connection.Open();
+
+                string sql = @"
+                SELECT 
+                    COUNT(*)   TOTAL_JUEGOS, 
+                    AVG(SCORE) PROMEDIO_PUNTAJE,
+                    MAX(SCORE) MEJOR_PUNTAJE,
+                    MIN(SCORE) PEOR_PUNTAJE,
+                FROM GAME_RESULTS
+                WHERE GAME_DATE >= ADD_MONTHS(SYSDATE, -6)";
+                               
+                OracleCommand cmd = new OracleCommand(sql, connection);
+                
+                var reader = cmd.ExecuteReader();
+                
+                while (reader.Read())
+                {
+                   
+                    Console.WriteLine("Total Juegos: " + reader["TOTAL_JUEGOS"]);
+                    Console.WriteLine("Promedio Puntuacion: " + reader["PROMEDIO_PUNTAJE"]);
+                    Console.WriteLine("Mejor Puntacion: " + reader["MEJOR_PUNTAJE"]);
+                    Console.WriteLine("Peor Puntuacion: " + reader["PEOR_PUNTAJE"]);
+                }
+            }
+        }
+    } 
 }
